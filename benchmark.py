33f6cd0fb6a5a5ed6b7737635d27c02c3e73470d
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-03-03 17:03:06 +0000

    Idle benchmark driver rewritten.

--- benchmark.py
+++ benchmark.py
@@ -1,55 +1,82 @@
-from pandas import DataFrame
+import os
+import argparse
+import json
+import platform
+
+from wrappers.PowerGadget import PowerGadget
+from browser import Browser
+from time import sleep
+from pandas import DataFrame, concat
 
 class Benchmark:
     def __init__(self, args):
-        self._debug = args.debug
-        self._duration = args.duration
-        self._iterations = args.iterations
-        self._output = args.output
+        self._args = args #browser and 
 
-    def _collect_power_usage(self):
-        df = DataFrame(columns=self._fields)
-
-        for i in range(0, self._iterations):
-            if self._debug:
-                print("Starting run", i)
-            df = self._run_iteration(df)
-
-        return df.convert_objects(convert_numeric=True)
-
-    def _run_iteration(self, df):
-        self.initialize_iteration()
-        self._tool.start()
-        self.execute_iteration()
-        summary = self._tool.join()
-        self.finalize_iteration()
-        return df.append(summary, ignore_index=True)
+        with open(args.config) as f:
+            self._config = json.load(f)
 
     def log(self):
-        self.initialize()
-        df = self._collect_power_usage()
-        df = self.filter_outliers(df)
-        df.describe().to_csv(self._output) if self._output else None
-        self.process_measurements(df)
-        self.finalize()
+        df = None
 
-    def initialize(self):
-        pass
+        for page in self._get_pages():
+            for browser in self._get_browsers():
+                browser = Browser.create_browser(name=browser["name"], path=browser["path"], page=page)
+                browser.initialize()
+                partial = None
+                sleep(self._args.sleep)
 
-    def initialize_iteration(self):
-        pass
+                for benchmark in self._get_benchmarks():
+                    benchmark = Benchmark._create_benchmark(benchmark, self._args)
+                    partial = self.run(benchmark, browser, partial)
 
-    def execute_iteration(self):
-        pass
+                browser.finalize()
+                df = partial if df is None else concat([df, partial])
 
-    def filter_outliers(self, df):
         return df
 
-    def finalize_iteration(self):
-        pass
+    def run(self, benchmark, browser, partial):
+        df = benchmark.log()
+        res = df if partial is None else partial.combine_first(df)
+        return res
 
-    def process_measurements(self, df):
-        pass
+    @staticmethod
+    def _create_benchmark(benchmark, args):
+        if benchmark == "PowerGadget":
+            return PowerGadget(args)
+        else:
+            raise Exception("Benchmark not found")
 
-    def finalize(self):
-        pass
+    def _get_pages(self):
+        return self._config["Pages"]
+
+    def _get_browsers(self):
+        os = platform.system()
+        return self._config["OS"][os]
+
+    def _get_benchmarks(self):
+        return self._config["Benchmarks"]
+
+if __name__ == "__main__":
+    parser= argparse.ArgumentParser(description="External browser benchmark suite",
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("-e", "--resolution", help="Sampling resolution in ms, if applicable", default=50, type=int)
+    parser.add_argument("-d", "--duration", help="Collection duration in s", default=30, type=int)
+    parser.add_argument("-i", "--iterations", help="Number of iterations", default=5, type=int)
+    parser.add_argument("-o", "--output_csv", help="Path of the final csv output", default="report.csv")
+    parser.add_argument("-l", "--plot", help="Path of the final plot, if applicable", default="report.png")
+    parser.add_argument("-p", "--path", help="Tool path", default="")
+    parser.add_argument("-b", "--benchmark", help="Benchmark to run", default="idle")
+    parser.add_argument("-c", "--config", help="Configuration file", default="config.json")
+    parser.add_argument("-s", "--sleep", help="Seconds to sleep before the benchmarks start recording", default=100, type=int)
+    parser.add_argument("--debug", help="Show debug messages", action="store_true")
+    args = parser.parse_args()
+
+    benchmark = None
+    if args.benchmark == "idle":
+        benchmark = Benchmark(args)
+    elif args.benchmark == "PowerGadget":
+        benchmark = PowerGadget(args)
+
+    df = benchmark.log()
+    df.to_csv(args.output_csv)
--- driver.py
+++ driver.py
@@ -1,5 +1,5 @@
 import argparse
-from benchmarks.PowerGadget import PowerGadgetBenchmark as PowerGadgetBenchmark
+from wrappers.PowerGadget import PowerGadget
 
 if __name__ == "__main__":
     parser= argparse.ArgumentParser(description="Plot Power Gadget's logs in time and frequency domain",
@@ -8,15 +8,16 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--resolution", help="Sampling resolution in ms, if applicable", default=50, type=int)
     parser.add_argument("-d", "--duration", help="Collection duration in s", default=30, type=int)
     parser.add_argument("-i", "--iterations", help="Number of iterations", default=5, type=int)
-    parser.add_argument("-o", "--output", help="Path of the final csv output", default="report.csv")
+    parser.add_argument("-o", "--output_csv", help="Path of the final csv output", default="report.csv")
     parser.add_argument("-l", "--plot", help="Path of the final plot, if applicable", default="report.png")
     parser.add_argument("-p", "--path", help="Tool path", default="")
-    parser.add_argument("-b", "--benchmark", help="Benchmark to run", default="power_gadget")
+    parser.add_argument("-b", "--benchmark", help="Wrapper to run", default="power_gadget")
     parser.add_argument("--debug", help="Show debug messages", action="store_true")
     args = parser.parse_args()
 
     benchmark = None
     if args.benchmark == "power_gadget":
-        benchmark = PowerGadgetBenchmark(args)
+        benchmark = PowerGadget(args)
 
-    benchmark.log()
+    df = benchmark.log()
+    df.to_csv(args.output_csv)
